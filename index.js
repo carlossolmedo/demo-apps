af5b564688e6e07d4adcdfd086479448d1172182
Carlos Olmedo <carlos.olmedodev@gmail.com>
2020-10-28 10:53:17 +0100

    feat(node): environment variables enable

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+require('dotenv').config({
+  path: `${__dirname}/.env.${process.env.NODE_ENV}`
+});
+
 const http = require('http');
 const hostname = 'localhost';
 const port = process.env.PORT;
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World!\n');
 });
+
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
